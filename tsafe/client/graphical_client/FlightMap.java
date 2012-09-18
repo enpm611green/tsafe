ed122a4b8fdc7eadeaeb9c437bd0cd51cd44716b
Mihai Mazilu <mtmazilu@gmail.com>
2012-09-18 12:03:48 -0400

    Added pause before showing map to avoid busy wait

--- tsafe/client/graphical_client/FlightMap.java
+++ tsafe/client/graphical_client/FlightMap.java
@@ -136,7 +136,13 @@ class FlightMap extends JComponent implements ImageObserver {
         // Prepare the map image
         this.imageReady = false;
         super.prepareImage(mapImage, this);
-        while(!this.imageReady);
+        while(!this.imageReady) {
+        	try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+        }
 
         // Set the size of the pane to the size of the image
         int width = mapImage.getWidth(this);
